8b4d2b826fa5a7c5acdbe1a5e77b9fa7fbcb4619
Ian J. Sikes <ianjsikes@gmail.com>
2015-06-25 15:28:32 -0700

    Added custom fractional noise, fixed ridged/inflected noise

--- Generators.py
+++ Generators.py
@@ -1,41 +1,65 @@
 __author__ = 'Ian'
 
-from noise import snoise2
+from noise import snoise2, pnoise2
 import math
 import MapUtility
 
 # Returns a 2D array of perlin noise values
 def generateNoise(resolution, offset, octaves, frequency):
     arr = list()
+    min = 1.0
+    max = 0.0
 
     for x in range(resolution):
         arr.append(list())
         for y in range(resolution):
             sample = (snoise2((x+offset[0]) / frequency, (y+offset[1]) / frequency, octaves)/2) + 0.5
+            if sample < min:
+                min = sample
+            elif sample > max:
+                max = sample
             sample = MapUtility.clamp(sample)
             arr[x].append(sample)
+    print "min: " + str(min)
+    print "max: " + str(max)
     return arr
 
 def generateInflectedNoise(resolution, offset, octaves, frequency):
     arr = list()
+    min = 1.0
+    max = 0.0
 
     for x in range(resolution):
         arr.append(list())
         for y in range(resolution):
-            sample = abs(snoise2((x+offset[0]) / frequency, (y+offset[1]) / frequency, octaves)/2)
+            sample = sumInflectedNoise((x+offset[0], y+offset[1]), frequency, octaves)
+            if sample < min:
+                min = sample
+            elif sample > max:
+                max = sample
             sample = MapUtility.clamp(sample)
             arr[x].append(sample)
+    print "min: " + str(min)
+    print "max: " + str(max)
     return arr
 
 def generateRidgedNoise(resolution, offset, octaves, frequency):
     arr = list()
+    min = 1.0
+    max = 0.0
 
     for x in range(resolution):
         arr.append(list())
         for y in range(resolution):
-            sample = 1.0 - abs(snoise2((x+offset[0]) / frequency, (y+offset[1]) / frequency, octaves)/2)
+            sample = sumRidgedNoise((x+offset[0], y+offset[1]), frequency, octaves)
+            if sample < min:
+                min = sample
+            elif sample > max:
+                max = sample
             sample = MapUtility.clamp(sample)
             arr[x].append(sample)
+    print "min: " + str(min)
+    print "max: " + str(max)
     return arr
 
 # Returns a 2D array with a radial gradient
@@ -51,4 +75,56 @@ def generateRadial(resolution, threshold, scale=1.0):
             #val = MapUtility.clamp(val)
             arr[x].append(val)
 
+    return arr
+
+def sumNoise(point, frequency, octaves=2, lacunarity=2.0, persistence=0.5):
+    sum = (snoise2(point[0] / frequency, point[1] / frequency) / 2.0) + 0.5
+    amplitude = 1.0
+    ran = 1.0
+    for o in range(1, octaves):
+        frequency /= lacunarity
+        amplitude *= persistence
+        ran += amplitude
+        sum += (snoise2(point[0] / frequency, point[1] / frequency) * 0.5 + 0.5) * amplitude
+    return sum / ran
+
+def sumInflectedNoise(point, frequency, octaves=2, lacunarity=2.0, persistence=0.5):
+    sum = abs(snoise2(point[0] / frequency, point[1] / frequency))
+    amplitude = 1.0
+    ran = 1.0
+    for o in range(1, octaves):
+        frequency /= lacunarity
+        amplitude *= persistence
+        ran += amplitude
+        sum += abs(snoise2(point[0] / frequency, point[1] / frequency)) * amplitude
+    return sum / ran
+
+def sumRidgedNoise(point, frequency, octaves=2, lacunarity=2.0, persistence=0.5):
+    sum = 1.0 - abs(snoise2(point[0] / frequency, point[1] / frequency))
+    amplitude = 1.0
+    ran = 1.0
+    for o in range(1, octaves):
+        frequency /= lacunarity
+        amplitude *= persistence
+        ran += amplitude
+        sum += (1.0 - abs(snoise2(point[0] / frequency, point[1] / frequency))) * amplitude
+    return sum / ran
+
+def generateFBMNoise(resolution, offset, octaves, frequency):
+    arr = list()
+    min = 1.0
+    max = 0.0
+
+    for x in range(resolution):
+        arr.append(list())
+        for y in range(resolution):
+            sample = sumNoise((x + offset[0], y + offset[1]), frequency, octaves)
+            if sample < min:
+                min = sample
+            elif sample > max:
+                max = sample
+            sample = MapUtility.clamp(sample)
+            arr[x].append(sample)
+    print "min: " + str(min)
+    print "max: " + str(max)
     return arr
--- LoadTest.py
+++ LoadTest.py
@@ -18,27 +18,14 @@ detailNoise = Generators.generateNoise(RESOLUTION, randomOffset, 6, 128.0)
 height = Modifiers.subtract(radial, noise)
 height = Modifiers.subtract(height, detailNoise)
 
-terrainMap = TerrainMap(height, 0.1)
-terrainMap.load_height_map("height.png")
-terrainMap.load_water_map("water.png")
-terrainMap.load_rainfall_map("rainfall.png")
-terrainMap.calculate_temperature()
-terrainMap.calculate_biomes()
+#terrainMap = TerrainMap(height, 0.1)
+
+fbmNoise = Generators.generateRidgedNoise(RESOLUTION, randomOffset, 6, 256.0)
+
+terrainMap = TerrainMap(fbmNoise, 0.0)
 
 img = terrainMap.get_terrain_map_image()
 img.save("test.png", "PNG")
 
 height_map = terrainMap.get_height_map_image()
 height_map.save("height.png", "PNG")
-
-water_map = terrainMap.get_water_map_image()
-water_map.save("water.png", "PNG")
-
-temperature_map = terrainMap.get_temperature_map_image()
-temperature_map.save("temperature.png", "PNG")
-
-rainfall_map = terrainMap.get_rainfall_map_image()
-rainfall_map.save("rainfall.png", "PNG")
-
-biome_map = terrainMap.get_biome_map_image()
-biome_map.save("biome.png", "PNG")
