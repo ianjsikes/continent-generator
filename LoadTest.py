5ff3396334e263fdfa5dc3b526852545617fdd5c
Ian J. Sikes <ianjsikes@gmail.com>
2015-06-25 19:04:09 -0700

    Added overlay function. Implemented ridged noise into heightmap.

--- LoadTest.py
+++ LoadTest.py
@@ -15,17 +15,24 @@ radial = Generators.generateRadial(RESOLUTION, 3.0, 2.0)
 randomOffset = (random.randint(-500, 500), random.randint(-500, 500))
 detailNoise = Generators.generateNoise(RESOLUTION, randomOffset, 6, 128.0)
 
-height = Modifiers.subtract(radial, noise)
-height = Modifiers.subtract(height, detailNoise)
+shape = Modifiers.subtract(radial, noise)
+height = Modifiers.subtract(shape, detailNoise)
 
 #terrainMap = TerrainMap(height, 0.1)
 
-fbmNoise = Generators.generateRidgedNoise(RESOLUTION, randomOffset, 6, 256.0)
+ridgedNoise = Generators.generateRidgedNoise(RESOLUTION, randomOffset, 3, 256.0)
 
-terrainMap = TerrainMap(fbmNoise, 0.0)
+ridgedNoise = Modifiers.scale(ridgedNoise, 0.75)
+ridgedHeight = Modifiers.overlay(ridgedNoise, height)
+
+terrainMap = TerrainMap(height, 0.1)
+ridgedMap = TerrainMap(ridgedHeight, 0.1)
 
 img = terrainMap.get_terrain_map_image()
 img.save("test.png", "PNG")
+
+ridged = ridgedMap.get_terrain_map_image()
+ridged.save("ridged.png", "PNG")
 
 height_map = terrainMap.get_height_map_image()
 height_map.save("height.png", "PNG")
--- Modifiers.py
+++ Modifiers.py
@@ -9,6 +9,20 @@ def scale(arr, factor):
             arr[x][y] = MapUtility.clamp(arr[x][y])
     return arr
 
+def invert(arr):
+    for x in range(len(arr)):
+        for y in range(len(arr[0])):
+            arr[x][y] = 1 - arr[x][y]
+            arr[x][y] = MapUtility.clamp(arr[x][y])
+    return arr
+
+def cutoff(arr, val):
+    for x in range(len(arr)):
+        for y in range(len(arr[0])):
+            if arr[x][y] < val:
+                arr[x][y] = 0
+    return arr
+
 def subtractConstant(arr, const):
     for x in range(len(arr)):
         for y in range(len(arr[0])):
@@ -23,6 +37,28 @@ def subtract(arrA, arrB):
         for y in range(len(arrA[0])):
             arrC[x].append(arrA[x][y] - arrB[x][y])
             arrC[x][y] = MapUtility.clamp(arrC[x][y])
+    return arrC
+
+def add(arrA, arrB):
+    arrC = list()
+    for x in range(len(arrA)):
+        arrC.append(list())
+        for y in range(len(arrA[0])):
+            arrC[x].append(arrA[x][y] + arrB[x][y])
+            arrC[x][y] = MapUtility.clamp(arrC[x][y])
+    return arrC
+
+def overlay(arrA, arrB):
+    arrC = list()
+    val = 0
+    for x in range(len(arrA)):
+        arrC.append(list())
+        for y in range(len(arrA[0])):
+            if arrB[x][y] < 0.5:
+                val = 2.0 * arrA[x][y] * arrB[x][y]
+            else:
+                val = 1.0 - (2.0 * (1 - arrA[x][y]) * (1 - arrB[x][y]))
+            arrC[x].append(val)
     return arrC
 
 def evaluate(arr, cutoff):
